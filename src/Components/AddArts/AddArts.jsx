31685adda15bb7c8b73e2aa9e38833cddf9760d2
Fahim2023 <fahimshahrier2023@gmail.com>
2025-11-09 23:04:07 +0600

    added AuthContext & AuthProvider

---
+++ src/Components/AddArts/AddArts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddArts = () => {
+  return <div></div>;
+};
+
+export default AddArts;
---
+++ src/Components/FeaturedArts/FeaturedArts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FeaturedArts = () => {
+  return <div></div>;
+};
+
+export default FeaturedArts;
